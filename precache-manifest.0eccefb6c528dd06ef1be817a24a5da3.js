2f370aa8de480b5928b2b2a0ee47f4ee90ef4a8f
NAgrawal1798 <agrawal.nikhil1798@gmail.com>
2020-07-02 23:44:37 +0530

    Updates

---
+++ precache-manifest.0eccefb6c528dd06ef1be817a24a5da3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57a85795ffc5a2eda5046ca5eba9445",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "8a63986eb59ae7e029fd",
+    "url": "/ToDoList/static/css/main.64584ac1.chunk.css"
+  },
+  {
+    "revision": "bd0c97b7a831b9122b2d",
+    "url": "/ToDoList/static/js/2.0512a152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList/static/js/2.0512a152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a63986eb59ae7e029fd",
+    "url": "/ToDoList/static/js/main.31271493.chunk.js"
+  },
+  {
+    "revision": "b3a82003f25af9c4504c",
+    "url": "/ToDoList/static/js/runtime-main.3d9c94f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
